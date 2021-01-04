e57eb5ee4ce54cef46813df8d16de7b6c7f18382
phukety <1252615137@qq.com>
2021-01-04 19:36:50 +0800

    update

--- cloud-client/src/main/java/cn/phukety/cloudclient/controller/HelloController.java
+++ cloud-client/src/main/java/cn/phukety/cloudclient/controller/HelloController.java
@@ -1,17 +1,17 @@
 package cn.phukety.cloudclient.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 @Slf4j
 @RestController
 public class HelloController {
-    @Autowired
+    @Resource
     private DiscoveryClient client;
 
     @GetMapping(value = "/hello")
--- cloud-ribbon-consumer/src/main/java/cn/phukety/cloudribbonconsumer/controller/ConsumerController.java
+++ cloud-ribbon-consumer/src/main/java/cn/phukety/cloudribbonconsumer/controller/ConsumerController.java
@@ -1,14 +1,15 @@
 package cn.phukety.cloudribbonconsumer.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.Resource;
+
 @RestController
 public class ConsumerController {
 
-    @Autowired
+    @Resource
     private RestTemplate restTemplate;
 
     @GetMapping("/ribbon-consumer")
